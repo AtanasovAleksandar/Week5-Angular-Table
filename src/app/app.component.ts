e82153fecd58c9f32e431ab78259796c4ea84f83
Aleksandar Atanasov <aleksandaratanasov43@gmail.com>
2019-01-05 23:52:29 +0100

    add and so edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  name;
+  address;
+  city;
+  pinCode;
+
+  userName;
+  userAddress
+  userCity
+  userPinCode
+
+  users = [];
+  user = { 'id': '', 'name': '', 'address': '', 'city': '', 'pincode': '' }
+  newObject: any;
+  object: any;
+
+  addUser() {
+    this.userName = this.name;
+    this.userAddress = this.address;
+    this.userCity = this.city;
+    this.userPinCode = this.pinCode;
+
+    this.user.name = this.userName;
+    this.user.address = this.userAddress
+    this.user.city = this.userCity;
+    this.user.pincode = this.userPinCode;
+
+    this.newObject = Object.assign({}, this.user)
+
+    this.users.push(this.newObject)
+    console.log(this.users);
+
+
+
+    this.name = '';
+    this.address = '';
+    this.city = '';
+    this.pinCode = '';
+
+    editUser()
+  }
+
+  editUser() {
+    for (var i = 0; i < this.users.length; i++) {
+      if (this.users[i].name == 'asd') {
+        let change = this.users[i]
+
+        change.name = 'ace';
+      }
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
